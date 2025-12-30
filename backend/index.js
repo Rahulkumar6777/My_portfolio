f0517624e360d05531fbd4278b6683f1bb403974
Rahulkumar6777 <rahulk48546@gmail.com>
2025-12-30 23:00:39 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
+app.set("trust proxy", true);
 app.use(express.json());
 
 
